f50c925fdec5deb86323b1068d1da34b2cbfc5be
Semih Dağhan <daghanex@hotmail.com>
2022-08-22 01:16:52 +0300

    Entity'lerimi oluşturdum

--- NetCoreDaghanDigitalApp/DaghanDigital.Core/Models/BaseEntity.cs
+++ NetCoreDaghanDigitalApp/DaghanDigital.Core/Models/BaseEntity.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DaghanDigital.Core
+namespace DaghanDigital.Core.Models
 {
     public abstract class BaseEntity
     {
---
+++ NetCoreDaghanDigitalApp/DaghanDigital.Core/Models/Entities/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaghanDigital.Core.Models.Entities
+{
+    public class Category:BaseEntity
+    {
+        public string CategoryName { get; set; }
+
+        //Navigation Property
+        public ICollection<Product> Products { get; set; }
+    }
+}
