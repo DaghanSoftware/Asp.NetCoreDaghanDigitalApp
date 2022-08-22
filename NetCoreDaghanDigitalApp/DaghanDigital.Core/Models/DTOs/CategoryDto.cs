ca7e309a97985452eccc933ad4f9da5a273ef7dc
Semih Dağhan <daghanex@hotmail.com>
2022-08-22 13:41:30 +0300

    DTOs
    DTO'lar oluşturuldu.

---
+++ NetCoreDaghanDigitalApp/DaghanDigital.Core/Models/DTOs/CategoryDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaghanDigital.Core.Models.DTOs
+{
+    public class CategoryDto:BaseDto
+    {
+        public string CategoryName { get; set; }
+    }
+}
