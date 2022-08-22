eafc5215a8f36aa5cc5694f9e910c1f7218809bf
Semih Dağhan <daghanex@hotmail.com>
2022-08-22 10:38:39 +0300

    Entitiy Configuration
    1.)Repository Katmanında Configuration isimli bir klasör oluşturdum ve Fluent Api ile entity nesnelerimizin configurationlarını yaptım
    2.) Birebir ve bireçok ilişkileri, tablodaki alanların maximum uzunluğu ve boş bırakılıp bırakılmayacağı gibi özellikleri tanımladım. Tablolalardaki primary keyleri IdentityColumn olarak işaretledik.
    3.)AppDbContext nesnemizin ModelBuilder ile oluşturduğumuz configürasyonları efcore'a bildirdim

---
+++ NetCoreDaghanDigitalApp/DaghanDigital.Repository/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using DaghanDigital.Core.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaghanDigital.Repository.Configurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(X => X.Id).UseIdentityColumn();
+            builder.Property(x => x.CategoryName).IsRequired().HasMaxLength(50);
+
+            builder.ToTable("Categories");
+
+        }
+    }
+}
---
+++ NetCoreDaghanDigitalApp/DaghanDigital.Repository/Configurations/ProductConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using DaghanDigital.Core.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaghanDigital.Repository.Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).UseIdentityColumn();
+            builder.Property(x => x.ProductName).IsRequired().HasMaxLength(200);
+            builder.Property(x => x.Stock).IsRequired();
+            builder.Property(x => x.Price).IsRequired().HasColumnType("decimal(18,2)");
+            //entity framework core da eğer ki isimlendirme kurallarını düzgün yapmasaydım category ile product arasındaki bireçok ilişkiyi aşağıdaki şekilde yazabilmekteyiz.
+            builder.HasOne(x => x.Category).WithMany(x => x.Products).HasForeignKey(x => x.CategoryId);
+
+            builder.ToTable("Products");
+        }
+    }
+}
