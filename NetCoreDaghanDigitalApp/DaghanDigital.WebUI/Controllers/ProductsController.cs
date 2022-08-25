ffaa6fdf96d7483eefab02cc97febb946a5b84c1
Semih Dağhan <daghanex@hotmail.com>
2022-08-25 18:12:14 +0300

    ProductController  ProductSave
    1.)MVC de veri tabanı bağlantısı yapıldı
    2.)Program.cs automapper implementasyonu yapıldı.
    3.)RepoServiceModule Configürasyonu yapıldı
    4.)Validasyon konfigürasyonu yapıldı
    MVC projesinde ProductController da ürünleri eklediğimiz bir Form tasarımı ve ürünleri ekleme işlemi yapan post işlemi yaptım.

--- NetCoreDaghanDigitalApp/DaghanDigital.WebUI/Controllers/ProductsController.cs
+++ NetCoreDaghanDigitalApp/DaghanDigital.WebUI/Controllers/ProductsController.cs
@@ -1,20 +1,52 @@
-﻿using DaghanDigital.Core.Services;
+﻿using AutoMapper;
+using DaghanDigital.Core.Models.DTOs;
+using DaghanDigital.Core.Models.Entities;
+using DaghanDigital.Core.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace DaghanDigital.WebUI.Controllers
 {
     public class ProductsController : Controller
     {
         private readonly IProductService _service;
-
-        public ProductsController(IProductService service)
+        private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
+        public ProductsController(IProductService service, ICategoryService categoryService, IMapper mapper)
         {
             _service = service;
+            _categoryService = categoryService;
+            _mapper = mapper;
         }
 
         public async Task<IActionResult> Index()
         {
             return View(await _service.GetProductWithCategory());
+        }
+
+        public async Task<IActionResult> Save()
+        {
+            var categories = await _categoryService.GetAllAsync();
+            var categoriesDto = _mapper.Map<List<CategoryDto>>(categories.ToList());
+            ViewBag.categories = new SelectList(categoriesDto, "Id", "CategoryName");
+
+            return View();
+
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> Save(ProductDto productDto)
+        {
+            if (ModelState.IsValid)
+            {
+                await _service.AddAsync(_mapper.Map<Product>(productDto));
+                return RedirectToAction(nameof(Index));
+            }
+            var categories = await _categoryService.GetAllAsync();
+            var categoriesDto = _mapper.Map<List<CategoryDto>>(categories.ToList());
+            ViewBag.categories = new SelectList(categoriesDto, "Id", "CategoryName");
+            return View();
+
         }
     }
 }
---
+++ NetCoreDaghanDigitalApp/DaghanDigital.WebUI/Modules/RepoServiceModule.cs
@@ -0,0 +1,40 @@
+﻿using Autofac;
+using DaghanDigital.Core.Repositories;
+using DaghanDigital.Core.Services;
+using DaghanDigital.Core.UnitOfWorks;
+using DaghanDigital.Repository;
+using DaghanDigital.Repository.Repositories;
+using DaghanDigital.Repository.UnitOfWorks;
+using DaghanDigital.Service.Mapping;
+using DaghanDigital.Service.Services;
+using System.Reflection;
+using Module = Autofac.Module;
+
+namespace DaghanDigital.WebUI.Modules
+{
+    public class RepoServiceModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterGeneric(typeof(GenericRepository<>)).As(typeof(IGenericRepository<>)).InstancePerLifetimeScope();
+            builder.RegisterGeneric(typeof(Service<>)).As(typeof(IService<>)).InstancePerLifetimeScope();
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>();
+
+
+
+            var apiAssembly = Assembly.GetExecutingAssembly();
+            var repoAssembly = Assembly.GetAssembly(typeof(AppDbContext));
+            var serviceAssembly = Assembly.GetAssembly(typeof(MapProfile));
+
+            builder.RegisterAssemblyTypes(apiAssembly, repoAssembly, serviceAssembly).Where(x => x.Name.EndsWith
+            ("Repository")).AsImplementedInterfaces().InstancePerLifetimeScope();
+
+            builder.RegisterAssemblyTypes(apiAssembly, repoAssembly, serviceAssembly).Where(x => x.Name.EndsWith
+            ("Service")).AsImplementedInterfaces().InstancePerLifetimeScope();
+
+
+
+        }
+    }
+
+}
