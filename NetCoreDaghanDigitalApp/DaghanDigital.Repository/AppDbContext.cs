bd306f8fd5b230f85505b6adeda70d147d3493a8
Semih Dağhan <daghanex@hotmail.com>
2022-08-25 19:59:44 +0300

    CreateDate/UpdateDate
    Her bir entity oluştururken veya güncellerken merkezi bir yerden ef core sayesinde Context üzerinde savechancge methodunu ezerek davranışını değiştiriyoruz.
    SaveChangeAsync ile cache de bekleyen verinin veritabanına yansıtma işlemi gerçekleşmeden önce update mi yoksa inster mi olduğunu anlayıp ona göre CreateDate veya UpdateDate alanlarını değiştiriyoruz.

--- NetCoreDaghanDigitalApp/DaghanDigital.Repository/AppDbContext.cs
+++ NetCoreDaghanDigitalApp/DaghanDigital.Repository/AppDbContext.cs
@@ -1,6 +1,9 @@
-﻿using DaghanDigital.Core.Models.Entities;
+﻿using DaghanDigital.Core.Models;
+using DaghanDigital.Core.Models.Entities;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace DaghanDigital.Repository
 {
@@ -12,6 +15,58 @@ namespace DaghanDigital.Repository
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductFeature> ProductFeatures { get; set; }
+
+        public override int SaveChanges()
+        {
+            foreach (var item in ChangeTracker.Entries())
+            {
+                if (item.Entity is BaseEntity entityReference)
+                {
+                    switch (item.State)
+                    {
+                        case EntityState.Added:
+                            {
+                                entityReference.CreateDate = System.DateTime.Now;
+                                break;
+                            }
+                        case EntityState.Modified:
+                            {
+                                Entry(entityReference).Property(x => x.CreateDate).IsModified = false;
+                                entityReference.UpdateDate = System.DateTime.Now;
+                                break;
+                            }
+                    }
+                }
+            }
+            return base.SaveChanges();
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var item in ChangeTracker.Entries())
+            {
+                if (item.Entity is BaseEntity entityReference)
+                {
+                    switch(item.State)
+                    {
+                        case EntityState.Added:
+                            {
+                                entityReference.CreateDate =System.DateTime.Now;
+                                break;
+                            }
+                        case EntityState.Modified:
+                            {
+                                Entry(entityReference).Property(x => x.CreateDate).IsModified = false;
+                                entityReference.UpdateDate = System.DateTime.Now;
+                                break;
+                            }
+                    }
+                }
+            }
+
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
